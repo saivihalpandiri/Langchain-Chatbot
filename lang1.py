8b52f50b1ef28524ab00c6681428bcac407952e5
saivihalpandiri <109639941+saivihalpandiri@users.noreply.github.com>
2025-07-29 14:55:03 +0530

    Update lang1.py

--- lang1.py
+++ lang1.py
@@ -2,7 +2,7 @@
 import os
 import streamlit as st
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.document_loaders import PyPDFLoader, UnstructuredWordDocumentLoader
+from langchain_community.document_loaders import PyPDFLoader, UnstructuredWordDocumentLoader
 from langchain.vectorstores import Chroma
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain.chains import RetrievalQA, ConversationalRetrievalChain
